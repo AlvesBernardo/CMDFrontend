b0069ad998cbd2df85e655015d84fbed8c161f76
AlvesBernardo <bernardo.correia.alves@student.nhlstenden.com>
2025-01-15 13:59:43 +0100

    testing

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/CMDFrontend/", // Replace 'CMDFrontend' with your repository name
   plugins: [react()],
   preview: {
     port: 8080,
