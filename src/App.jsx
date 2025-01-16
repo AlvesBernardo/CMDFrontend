25e2f8fa248fe778e1d0a2ed6dca80ead55f0cf8
AlvesBernardo <bernardo.correia.alves@student.nhlstenden.com>
2025-01-16 14:57:26 +0100

    testing again

--- src/App.jsx
+++ src/App.jsx
@@ -16,19 +16,19 @@ import AdminResults from "./screens/AdminResults/AdminResults.jsx";
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/CMDFrontend"> {/* Set the basename here */}
             <ToastContainer />
             <Routes>
                 <Route path="/" element={<SignIn />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route element={<ProtectedRoute />}>
-                    <Route element={<AdminRoutes/>}>
+                    <Route element={<AdminRoutes />}>
                         <Route path="/adminDashboard" element={<AdminDashboard />} />
                         <Route path="/manageStudios" element={<ManageStudios />} />
                         <Route path="/studentList" element={<StudentList />} />
                         <Route path="/adminResults" element={<AdminResults />} />
                     </Route>
-                    <Route element={<StudentRoutes/>}>
+                    <Route element={<StudentRoutes />}>
                         <Route path="/studentDashboard" element={<StudentDashboard />} />
                         <Route path="/questionnaire" element={<Questionnaire />} />
                         <Route path="/studentresults" element={<StudentResults />} />
